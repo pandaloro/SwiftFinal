9e72a40725cab7a868d6223e8bb8ee14488f4d51
pandaloro <136196122+pandaloro@users.noreply.github.com>
2024-04-30 17:47:58 -0400

    Create FinalProjectApp.swift

---
+++ FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by Peter Andaloro on 4/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
